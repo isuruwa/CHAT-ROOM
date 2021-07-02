729b13f4243ef84e2366f9de5b099f0318d69a74
Devil Master <72663288+isuruwa@users.noreply.github.com>
2021-07-02 20:48:57 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,67 @@
+import sys, time, socket, threading, os
+
+os.system('clear')
+
+class colors:
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    red = '\033[31m'
+    pink = '\033[35m'
+
+
+banner = colors.cyan + """
+  _______ _____ ______    ___  ____  ____  __  ___
+ / ___/ // / _ /_  __/___/ _ \/ __ \/ __ \/  |/  /
+/ /__/ _  / __ |/ / /___/ , _/ /_/ / /_/ / /|_/ /
+\___/_//_/_/ |_/_/     /_/|_|\____/\____/_/  /_/
+
+ [*] CHAT-ROOM - A SIMPLE CHAT SERVER [*]
+ [*]    DEVELOPED BY DEVIL MASTER   [*]
+
+"""
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1 / 10)
+
+
+print(banner)
+slowprint(colors.pink + "[CONNECTING] Connecting to server..." )
+slowprint(colors.green + "[CONNECTED] Connected to server...")
+nickname = input(colors.blue + "[+] Enter your username : ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = '127.0.0.1'
+port = 5555
+client.connect((server, port))
+
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICKNAME':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            slowprint(bcolor.red + "[ERROR] An error occured!")
+            client.close()
+            break
+
+
+def write():
+    while True:
+        message = '[{}] >> {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
---
+++ server.py
@@ -0,0 +1,85 @@
+import sys, time, socket, threading, os
+
+os.system('clear')
+
+
+class colors:
+    blue = '\033[94m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    red = '\033[31m'
+    pink = '\033[35m'
+
+
+banner = colors.cyan + """
+  _______ _____ ______    ___  ____  ____  __  ___
+ / ___/ // / _ /_  __/___/ _ \/ __ \/ __ \/  |/  /
+/ /__/ _  / __ |/ / /___/ , _/ /_/ / /_/ / /|_/ /
+\___/_//_/_/ |_/_/     /_/|_|\____/\____/_/  /_/
+
+ [*] CHAT-ROOM - A SIMPLE CHAT SERVER [*]
+ [*]   DEVELOPED BY DEVIL MASTER    [*]
+
+"""
+host = '127.0.0.1'
+port = 5555
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1 / 10)
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(
+                '{} left in the chatroom!\n'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        slowprint(colors.green +
+                  "[CONNECTED] Connected with {}".format(str(address)))
+        client.send('NICKNAME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        slowprint("[CLIENT] {}".format(nickname))
+        broadcast("[CLIENT] {} joined in the chatroom!".format(
+            nickname).encode('ascii'))
+        #client.send('[!] Connected to the server...\n'.encode('ascii'))
+        #client.send('\n[+] You can now start chatting...'.encode('ascii'))
+        thread = threading.Thread(target=handle, args=(client, ))
+        thread.start()
+
+
+print(banner)
+slowprint(colors.green + "[STARTED] Server is runing...")
+slowprint(colors.pink + "[WAITING] Waiting for clients ...")
+receive()
+
